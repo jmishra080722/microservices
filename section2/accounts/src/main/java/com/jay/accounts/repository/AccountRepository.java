567aae8257575f06487d98a619cc3a25928a8344
Jayhind Mishra <jmishra080722@gmail.com>
2024-05-04 16:58:22 +0530

    ms-v1:Created entity and repository classes

---
+++ section2/accounts/src/main/java/com/jay/accounts/repository/AccountRepository.java
@@ -0,0 +1,9 @@
+package com.jay.accounts.repository;
+
+import com.jay.accounts.entity.Accounts;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AccountRepository extends JpaRepository<Accounts, Long> {
+}
---
+++ section2/accounts/src/main/java/com/jay/accounts/repository/CustomerRepository.java
@@ -0,0 +1,9 @@
+package com.jay.accounts.repository;
+
+import com.jay.accounts.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+}
