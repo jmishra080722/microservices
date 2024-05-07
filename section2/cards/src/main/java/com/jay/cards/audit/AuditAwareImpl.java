43cf0888bef6d4709d7a45e1c8e6d07e981c03f6
Jayhind Mishra <jmishra080722@gmail.com>
2024-05-07 13:06:51 +0530

    ms-v1: Cards MS

--- section2/cards/src/main/java/com/jay/cards/audit/AuditAwareImpl.java
+++ section2/cards/src/main/java/com/jay/cards/audit/AuditAwareImpl.java
@@ -1,2 +1,21 @@
-package com.jay.cards.audit;public class AuditAwareImpl {
+package com.jay.cards.audit;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component("auditAwareImpl")
+public class AuditAwareImpl implements AuditorAware<String> {
+
+    /**
+     * Returns the current auditor of the application.
+     *
+     * @return the current auditor.
+     */
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        return Optional.of("CARDS_MS");
+    }
+
 }
--- section2/cards/src/main/java/com/jay/cards/exception/CardAlreadyExistsException.java
+++ section2/cards/src/main/java/com/jay/cards/exception/CardAlreadyExistsException.java
@@ -1,2 +1,13 @@
-package com.jay.cards.exception;public class CardAlreadyExistsException {
+package com.jay.cards.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.BAD_REQUEST)
+public class CardAlreadyExistsException extends RuntimeException {
+
+    public CardAlreadyExistsException(String message){
+        super(message);
+    }
+
 }
