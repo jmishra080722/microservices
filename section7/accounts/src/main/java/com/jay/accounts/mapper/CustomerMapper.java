a00b7391efed9ed96fdf2687b1624a4d0e667673
Jayhind Mishra <jmishra080722@gmail.com>
2024-05-20 13:53:43 +0530

    ms-v1:Initial version of code to change DB from H2 to MySQL

---
+++ section7/accounts/src/main/java/com/jay/accounts/mapper/CustomerMapper.java
@@ -0,0 +1,21 @@
+package com.jay.accounts.mapper;
+
+import com.jay.accounts.dto.CustomerDto;
+import com.jay.accounts.entity.Customer;
+
+public class CustomerMapper {
+
+    public static CustomerDto mapToCustomerDto(Customer customer, CustomerDto customerDto) {
+        customerDto.setName(customer.getName());
+        customerDto.setEmail(customer.getEmail());
+        customerDto.setMobileNumber(customer.getMobileNumber());
+        return customerDto;
+    }
+
+    public static Customer mapToCustomer(CustomerDto customerDto, Customer customer) {
+        customer.setName(customerDto.getName());
+        customer.setEmail(customerDto.getEmail());
+        customer.setMobileNumber(customerDto.getMobileNumber());
+        return customer;
+    }
+}
---
+++ section7/loans/src/main/java/com/jay/loans/service/ILoansService.java
@@ -0,0 +1,14 @@
+package com.jay.loans.service;
+
+import com.jay.loans.dto.LoansDto;
+
+public interface ILoansService {
+
+    void createLoan(String mobileNumber);
+
+    LoansDto fetchLoan(String mobileNumber);
+
+    boolean updateLoan(LoansDto loansDto);
+
+    boolean deleteLoan(String mobileNumber);
+}
