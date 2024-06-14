596e9166dfced078821cb3650e628dba6145b7d2
Jayhind Mishra <jmishra080722@gmail.com>
2024-06-14 10:57:24 +0530

    ms-v1: Update fallback message for cards MS, Lecture-138

--- section10/accounts/src/main/java/com/jay/accounts/service/client/CardsFallback.java
+++ section10/accounts/src/main/java/com/jay/accounts/service/client/CardsFallback.java
@@ -1,6 +1,7 @@
 package com.jay.accounts.service.client;
 
 import com.jay.accounts.dto.CardsDto;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
@@ -9,6 +10,6 @@ public class CardsFallback implements CardsFeignClient {
 
     @Override
     public ResponseEntity<CardsDto> fetchCardDetails(String correlationId, String mobileNumber) {
-        return null;
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new CardsDto());
     }
 }
