5d06190d27f2e1f8f0fa38806ad1f9c9326c289c
Jayhind Mishra <jmishra080722@gmail.com>
2024-06-22 09:49:00 +0530

    ms-v1: Added RateLimiter to Account MS, Lecture-147

--- section10/accounts/src/main/java/com/jay/accounts/controller/AccountsController.java
+++ section10/accounts/src/main/java/com/jay/accounts/controller/AccountsController.java
@@ -8,6 +8,7 @@ import com.jay.accounts.dto.ResponseDto;
 import com.jay.accounts.entity.Accounts;
 import com.jay.accounts.entity.Customer;
 import com.jay.accounts.service.IAccountsService;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -242,11 +243,18 @@ public class AccountsController {
             )
     }
     )
+    @RateLimiter(name= "getJavaVersion", fallbackMethod = "getJavaVersionFallback")
     @GetMapping("/java-version")
     public ResponseEntity<String> getJavaVersion(){
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(environment.getProperty("JAVA_HOME"));
+    }
+
+    public ResponseEntity<String> getJavaVersionFallback(Throwable throwable){
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body("Java 17");
     }
 
     @Operation(
