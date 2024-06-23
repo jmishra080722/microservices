95a6c2b6898de09a206eb5fec7bbc638ca1951b1
Jayhind Mishra <jmishra080722@gmail.com>
2024-06-23 16:31:00 +0530

    ms-v1: Changes to make Gatewayserver as resourceServer, Lecture-187

---
+++ section_12/gatewayserver/src/main/java/com/jay/gatewayserver/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.jay.gatewayserver.config;
+
+
+import jakarta.ws.rs.HttpMethod;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity serverHttpSecurity){
+
+        serverHttpSecurity.authorizeExchange(exchange -> exchange.pathMatchers(HttpMethod.GET).permitAll()
+                .pathMatchers("/eazybank/accounts/**").authenticated()
+                .pathMatchers("/eazybank/loans/**").authenticated()
+                .pathMatchers("/eazybank/cards/**").authenticated())
+                .oauth2ResourceServer(oAuth2ResourceServerSpec -> oAuth2ResourceServerSpec
+                        .jwt(Customizer.withDefaults()));
+
+        serverHttpSecurity.csrf(csrfSpec -> csrfSpec.disable());
+        return serverHttpSecurity.build();
+
+
+    }
+}
